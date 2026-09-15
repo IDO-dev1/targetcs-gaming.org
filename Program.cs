a925022a9b12279affd17d433bd4709828fc66ef
blu-knite <75612535+blu-knite@users.noreply.github.com>
2026-09-15 20:41:24 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Windows.Forms;
+using TargetCS_Gaming.org.Helpers;
 
 namespace TargetCS_Gaming.org;
 
 internal static class Program
 {
     [STAThread]
-    static void Main()
+    static void Main(string[] args)
     {
         ApplicationConfiguration.Initialize();
-        Application.Run(new MainForm());
+
+        var silent = CommandLineParser.HasFlag(args, "--silent");
+        var startup = CommandLineParser.HasFlag(args, "--startup");
+        var exitAfterUpdate = CommandLineParser.HasFlag(args, "--exit-after-update");
+
+        Application.Run(new MainForm(
+            silentMode: silent,
+            startupMode: startup,
+            exitAfterUpdate: exitAfterUpdate));
     }
 }
