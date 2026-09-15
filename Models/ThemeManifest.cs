ea87f60d189d3715ff4ef6521e0ee2cdcb4c42f0
blu-knite <75612535+blu-knite@users.noreply.github.com>
2026-09-15 20:33:03 +0530

    Create ThemeManifest.cs

---
+++ Models/ThemeManifest.cs
@@ -0,0 +1,8 @@
+namespace TargetCS_Gaming.org.Models;
+
+public class ThemeManifest
+{
+    public string Name { get; set; } = "default";
+    public string BackgroundUrl { get; set; } = "";
+    public string AccentColor { get; set; } = "#FFFFFF";
+}
