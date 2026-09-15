7c9c390c49d17ccbc9ca4dcdcbff48a544bf6a90
blu-knite <75612535+blu-knite@users.noreply.github.com>
2026-09-15 20:36:45 +0530

    Create StartupOptions.cs

---
+++ Models/StartupOptions.cs
@@ -0,0 +1,8 @@
+namespace TargetCS_Gaming.org.Models;
+
+public class StartupOptions
+{
+    public bool AutoStart { get; set; }
+    public bool SilentMode { get; set; }
+    public bool ExitAfterUpdate { get; set; }
+}
